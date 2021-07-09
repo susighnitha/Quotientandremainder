b8eeeb22def1fc1699921244334da003616a0bfa
susighnitha <susighnithareddy00@gmail.com>
2021-07-09 08:15:48 +0530

    Quotientandremainder

---
+++ src/QuotientandRemainder.java
@@ -0,0 +1,18 @@
+
+public class QuotientandRemainder {
+	
+		   public static void main(String args[]){
+		     
+		    
+		      int num1= 15;
+		      int num2= 2;
+		     
+
+		      int quotient = num1 / num2;
+		      int remainder = num1 % num2;
+
+		      System.out.println("Quotient value:"+quotient);
+		      System.out.println("Remainder value:"+remainder);
+		   }
+		}
+
